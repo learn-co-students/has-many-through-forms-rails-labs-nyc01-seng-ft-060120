a03fa7e0a9eb3cdbc609c19d99d9c242ce3b1ddb
mayalexandera <maya.blevins@gmail.com>
2020-06-26 15:35:55 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+3.times do |cat|
+  Category.create(
+    name: "category #{cat}"
+  )
+end
+
+3.times do |post|
+  Post.create(
+    title: "post title #{post}",
+    content: "post content #{post}"
+  )
+end
+
+PostCategory.create(
+  post_id: Post.first.id, 
+  category_id: Category.first.id
+)
+
+PostCategory.create(
+  post_id: Post.second.id, 
+  category_id: Category.first.id
+)
+PostCategory.create(
+  post_id: Post.third.id, 
+  category_id: Category.first.id
+)
+
+
+User.create(
+  username: "Amanda",
+  email: "amanda@amanga.com"
+)
+
+3.times do |comment|
+  Comment.create(
+    content: "comment content #{comment}",
+    user_id: User.first.id,
+    post_id: Post.first.id
+  )
+end
